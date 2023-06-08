e7f7c252e1eb2433abf59a513d0f71f4b460d9ba
AleksK43 <117122687+AleksK43@users.noreply.github.com>
2023-06-08 21:33:30 +0200

    update

--- src/app/Guards/auth.guard.ts
+++ src/app/Guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../Services/auth.service';
 import { NgToastService } from 'ng-angular-popup';
@@ -7,7 +7,7 @@ import { NgToastService } from 'ng-angular-popup';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private auth: AuthService, private router: Router, private toast: NgToastService){
 
   }
